ac182bfebb65a8c6f38f32f058f799963a8a93b1
Andrii Yelnikov <whiteyelnikov@gmail.com>
2022-08-31 19:22:11 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
   const [data, setData] = useState();
   const [query, setQuery] = useState('');
+
   useEffect( () => { 
     async function fetchData() {
         try {
@@ -13,36 +14,48 @@ function App() {
         } catch (err) {
             console.log(err);
         }
-    }
+    };
+
     fetchData();
-}, []);
+  }, []);
 
 const filterData = (input) => {
   if (data !== undefined && data.length !== 0) {
-    const filtered = [...data].filter((el) => el.title !== undefined && el.title.toLowerCase().includes(input.toLowerCase()));
+    const filtered = [...data].filter((obj) => obj.title !== undefined && obj.title.toLowerCase().includes(input.toLowerCase()));
     return filtered;
   }
 }
 
 const filteredData = filterData(query);
   return (
-    <div className="App">
-      <form>
-        <input 
+    <div className="main">
+      <div className='main__block'>
+      <form className='main__form'>
+        <label className='main__form-label'>filter by title</label>
+        <input
+          className='main__form-input'
+          placeholder='title'
           onChange={(event) => {
             setQuery(event.target.value);
             filterData(query);
           }}/>
       </form>
-      <ul>
+      <ul className='main__list'>
       {filteredData !== undefined && filteredData.map(el => (
         <li
-          key={el['@id']}>
-            {`${el['@path']}/`}<a href={`/${el.title}`}>
-          {`${el.title}`}</a>
+          className='main__list-item'
+          key={el['@id']}
+        > {`${el['@path']}/`}
+        <a 
+          className='main__link'
+          href={`/${el.title}`}
+        >
+          {`${el.title}`}
+        </a>
         </li>
       ))}
       </ul>
+      </div>
     </div>
   );
 }
