d921bfc799a96ae2cd9a97d7bf4c0acec8333d40
Andrii Yelnikov <whiteyelnikov@gmail.com>
2022-08-31 15:25:57 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { getData } from './api';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState();
+  const [query, setQuery] = useState('');
+  useEffect( () => { 
+    async function fetchData() {
+        try {
+            const res = await getData(); 
+            setData(res.results);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    fetchData();
+}, []);
+
+const filterData = (input) => {
+  if (data !== undefined && data.length !== 0) {
+    const filtered = [...data].filter((el) => el.title !== undefined && el.title.toLowerCase().includes(input.toLowerCase()));
+    console.log(data)
+    return filtered;
+  }
+}
+
+const filteredData = filterData(query);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input 
+          onChange={(event) => {
+            setQuery(event.target.value);
+            filterData(query);
+          }}/>
+      </form>
+      <ul>
+      {filteredData !== undefined && filteredData.map(el => (
+        <li
+          key={el['@id']}>
+          <a href={el.title || '/'}>
+          {`${el.title || '-'}`}
+          </a>
+        </li>
+      ))}
+      </ul>
     </div>
   );
 }
